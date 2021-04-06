cd7a55281c4a8c1884ba1f2fa6f984c9e0825775
Shota Ray Imaki <shota.imaki.0801@gmail.com>
2021-04-06 14:30:45 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!bin/sh -eu
 
 cd $HOME
-git clone https://github.com/simaki/dotfiles.git
+git clone https://github.com/simaki/dotfiles.git && cd dotfiles && git checkout sync
+
 ln -fs $HOME/dotfiles/update.sh $HOME/update.sh
 sh $HOME/dotfiles/brew/setup_brew.sh
 find $HOME/dotfiles -type f -name setup.sh -exec sh "{}" \;
