e7fa5382dccd60a01714d6b0301cbda8bc0b8197
Shota Imaki <shota.imaki.0801@gmail.com>
2021-04-12 15:42:15 +0900

    sync

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -4,6 +4,13 @@ echo '--- VSCode setup start ---'
 
 brew install --cask visual-studio-code
 
+# Shell Command: Install 'code' command in PATH
+# https://code.visualstudio.com/docs/setup/mac
+cat << EOF >> ~/.zprofile
+# Add Visual Studio Code (code)
+export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+EOF
+
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
 
