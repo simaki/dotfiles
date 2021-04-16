c4c093377cbd0a7150899dd71ff4fe90206ae79a
Shota Imaki <shota.imaki.0801@gmail.com>
2021-04-16 12:47:46 +0900

    sync

--- git/setup.sh
+++ git/setup.sh
@@ -2,10 +2,10 @@
 
 cd `dirname $0`
 
-ln -fs $HOME/dotfiles/git/.gitconfig $HOME
+ignore=$HOME/dotfiles/git/ignore
+touch $ignore
+curl -fsS https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore >> $ignore
+curl -fsS https://raw.githubusercontent.com/github/gitignore/master/Global/Vim.gitignore >> $ignore
+curl -fsS https://raw.githubusercontent.com/github/gitignore/master/Global/VisualStudioCode.gitignore >> $ignore
 
-touch $HOME/.gitignore_global
-curl -fsS https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore >> $HOME/.gitignore_global
-curl -fsS https://raw.githubusercontent.com/github/gitignore/master/Global/Vim.gitignore >> $HOME/.gitignore_global
-curl -fsS https://raw.githubusercontent.com/github/gitignore/master/Global/VisualStudioCode.gitignore >> $HOME/.gitignore_global
-git config --global core.excludesfile $HOME/.gitignore_global
+ln -fs $ignore $HOME/.config/git
