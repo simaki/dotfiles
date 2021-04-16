7ff68ce5d71b26f83d4e5f73a0d13974c6ad04e3
Shota Imaki <shota.imaki.0801@gmail.com>
2021-04-16 12:06:21 +0900

    sync

--- brew/setup_brew.sh
+++ brew/setup_brew.sh
@@ -5,10 +5,8 @@ cd `dirname $0`
 echo "--- Homebrew setup start ---"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" && brew --version
-xargs brew install < brew.txt
-xargs brew tap < tap.txt
-xargs brew install --cask < cask.txt
 
 brew install cowsay && cowsay 'Hello, Homebrew!'
+brew bundle
 
 echo "--- Homebrew setup end ---"
