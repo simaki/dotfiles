1a8a045782182315311de25e5504a6454b173e7f
Shota Imaki <shota.imaki.0801@gmail.com>
2021-05-07 14:11:20 +0900

    sync

--- python/update.sh
+++ python/update.sh
@@ -1,14 +1,9 @@
 #!bin/sh -eu
 
-echo "--- pip update start ---"
+echo '--- start Python update ---'
 
 pip list --format freeze | sed 's/==.*//g' | xargs pip install --upgrade --quiet
 # sed -i -n "s/__requires__ = 'pip==.*'/__requires__ = 'pip'/" `which pip3`
-
-echo "--- pip update end ---"
-
-echo "--- Jupyter update start ---"
-
 jupyter labextension update --all --Application.log_level=40
 
-echo "--- Jupyter update end ---"
+echo '--- end Python update ---'
--- rust/setup.sh
+++ rust/setup.sh
@@ -4,5 +4,6 @@ echo '--- setup Rust start ---'
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustc hello.rs && ./hello && rm ./hello
+ln -fs $HOME/dotfiles/rust/rustrun /usr/local/bin
 
 echo '--- setup Rust end ---'
