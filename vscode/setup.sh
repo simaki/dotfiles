85ad8af63397a867448f8185ba934046de405d40
Shota Imaki <shota.imaki.0801@gmail.com>
2021-07-01 10:07:36 +0900

    sync

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -2,12 +2,6 @@
 
 echo '--- VSCode setup start ---'
 
-# Shell Command: Install 'code' command in PATH https://code.visualstudio.com/docs/setup/mac
-cat << EOF >> ~/.zprofile
-# Add Visual Studio Code (code)
-export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-EOF
-
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 
 ln -fs $HOME/dotfiles/vscode/snippets/* $HOME/Library/Application\ Support/Code/User/snippets
