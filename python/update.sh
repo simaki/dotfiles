152c22e3bac4e904715fe92d99b1dce8814d8cff
Shota Imaki <shota.imaki.0801@gmail.com>
2021-07-07 17:12:54 +0900

    sync

--- python/update.sh
+++ python/update.sh
@@ -1,5 +1,6 @@
 #!bin/sh -eu
 
+pip install --upgrade --quiet pip
 pip list --format freeze | sed 's/==.*//g' | xargs pip install --upgrade --quiet
 # sed -i -n "s/__requires__ = 'pip==.*'/__requires__ = 'pip'/" `which pip3`
 jupyter labextension update --all --Application.log_level=40
